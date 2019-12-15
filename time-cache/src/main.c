d05fc94a4babbb331a0abca39169e9a4df3525b9
Jia ZHENG <j.zheng1994@gmail.com>
2019-12-16 04:11:15 +0800

    Finish single thread

--- time-cache/src/main.c
+++ time-cache/src/main.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <errno.h>
 #include <pthread.h>
 #include <stdio.h>
@@ -5,6 +6,63 @@
 #include <unistd.h>
 
 #include "cache.h"
+
+void check_cache(TimeCache *cache)
+{
+}
+
+char* strip(char *input_string)
+{
+  while(isspace((unsigned char)* input_string))
+      input_string++;
+
+  if (*input_string == 0)
+      return input_string;
+
+  char *end;
+  end = input_string + strlen(input_string) - 1;
+  while(end > input_string && isspace((unsigned char) *end)) 
+      end--;
+  end[1] = '\0';
+
+  return input_string;
+}
+
+int read_stdin(TimeCache *time_cache)
+{
+    size_t buffer_size = 128;
+    size_t chars_read;
+    char *buffer = (char*) malloc(buffer_size * sizeof(char));
+    char *stripped;
+    int cache_item;
+
+    if (buffer == NULL)
+    {
+        perror("Unable to allocate buffer to read stdin inputs.");
+        return EXIT_FAILURE;
+    }
+
+    // Read input from command line
+    while (true) {
+        printf("Enter an item ID to cache:\n");
+        chars_read = getline(&buffer, &buffer_size, stdin);
+        if (chars_read > 1)
+        {
+            stripped = strip(buffer);
+            cache_item = strtoul(stripped, NULL, 10);
+            bool success = add_item_to_timecache(time_cache, cache_item);
+            printf("Add item %i %s.\n", cache_item, 
+                                        success ? "success" : "failed");
+            list_timecache_info(time_cache);
+        }
+        else
+        {
+            printf("Empty input, terminating program.\n");
+            break;
+        }
+    }
+    return EXIT_SUCCESS;
+}
 
 int main(int argc, char *argv[])
 {
@@ -26,12 +84,10 @@ int main(int argc, char *argv[])
     }
 
     printf("Initialising cache with capacity %d and TTL %d.\n", capacity, ttl);
-    TimeCache* timecache = create_timecache(capacity, ttl);
-    bool success = add_item_to_timecache(timecache, 1);
-    printf("Add item %i %s.\n", 1, success ? "success" : "failed");
-    list_timecache_info(timecache);
+    TimeCache* time_cache = create_timecache(capacity, ttl);
+    read_stdin(time_cache);
     printf("Freeing cache.\n");
-    free_timecache(timecache);
+    free_timecache(time_cache);
 
     return EXIT_SUCCESS;
 }
